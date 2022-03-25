8385dc2b60f23b3acfeddbcaab6c422ba5fc6a80
DashaShabalina <67578446+DashaShabalina@users.noreply.github.com>
2022-03-25 17:52:40 +0700

    Add files via upload

---
+++ Task1.Data/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1.Data
+{
+    public class Point
+    {
+        public int Id { get; set; }
+        public int IdEquipment { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public Point(int id, int idEquipment, double latitude, double longitude)
+        {
+            Id = id;
+            IdEquipment = idEquipment;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
